80b00e657966bc92459df18442f1a1caacd1c70e
eloritzkovitz <elor2200@gmail.com>
2025-09-08 10:05:15 +0300

    Added logging

--- src/utils/firebaseMessaging.ts
+++ src/utils/firebaseMessaging.ts
@@ -2,14 +2,17 @@ import { JWT } from "google-auth-library";
 import axios from "axios";
 import fs from "fs";
 import path from "path";
-import 'dotenv/config';
+import "dotenv/config";
+import logger from "./logger";
 
 const SCOPES = ["https://www.googleapis.com/auth/firebase.messaging"];
 
 // Load service account from GOOGLE_APPLICATION_CREDENTIALS env variable
 const credentialsPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 if (!credentialsPath) {
-  throw new Error("GOOGLE_APPLICATION_CREDENTIALS environment variable is not set.");
+  throw new Error(
+    "GOOGLE_APPLICATION_CREDENTIALS environment variable is not set."
+  );
 }
 const serviceAccount = JSON.parse(
   fs.readFileSync(path.resolve(credentialsPath), "utf8")
@@ -23,6 +26,7 @@ const jwtClient = new JWT({
   scopes: SCOPES,
 });
 
+// Send FCM message using HTTP v1 API
 export async function sendFcmHttpV1({
   token,
   notification,
@@ -36,27 +40,27 @@ export async function sendFcmHttpV1({
   android?: any;
   webpush?: any;
 }) {
-  await jwtClient.authorize();
-  const accessToken = jwtClient.credentials.access_token;
+  try {
+    await jwtClient.authorize();
+    const accessToken = jwtClient.credentials.access_token;
 
-  const message = {
-    message: {
-      token,
-      notification,
-      data,
-      android,
-      webpush,      
-    },
-  };
+    const message: any = { message: { token, notification } };
+    if (data) message.message.data = data;
+    if (android) message.message.android = android;
+    if (webpush) message.message.webpush = webpush;
 
-  await axios.post(
-    `https://fcm.googleapis.com/v1/projects/${PROJECT_ID}/messages:send`,
-    message,
-    {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        "Content-Type": "application/json",
-      },
-    }
-  );
+    await axios.post(
+      `https://fcm.googleapis.com/v1/projects/${PROJECT_ID}/messages:send`,
+      message,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error: any) {
+    logger.error("FCM send error: %o", error?.response?.data || error.message);
+    throw error;
+  }
 }
